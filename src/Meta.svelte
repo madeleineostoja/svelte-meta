6b9e9db9d63427a3bc1ddaa298151784bf6c0782
Sean King <sean@seanking.co>
2020-11-02 13:41:21 +1300

    init

---
+++ src/Meta.svelte
@@ -0,0 +1,32 @@
+<!--
+  Meta Component
+  Metadata helper
+-->
+<script>
+  /** Page title */
+  export let title: Optional<string> = undefined;
+  /** Page description */
+  export let description: Optional<string> = undefined;
+  /** Page image */
+  export let image: Optional<string> = undefined;
+</script>
+
+<svelte:head>
+  {#if title}
+    <title>{title}</title>
+    <meta name="title" content={title} />
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+  {/if}
+
+  {#if description}
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+  {/if}
+
+  {#if image}
+    <meta property="og:image" content={image} />
+    <meta name="twitter:image" content={image} />
+  {/if}
+</svelte:head>
