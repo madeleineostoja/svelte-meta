758397b6a74d6ef6502184063c73fd43a6c98e66
Sean King <sean@seanking.co>
2020-11-02 13:50:58 +1300

    Remove unecessary twitter: meta tags

--- src/Meta.svelte
+++ src/Meta.svelte
@@ -16,17 +16,14 @@
     <title>{title}</title>
     <meta name="title" content={title} />
     <meta property="og:title" content={title} />
-    <meta name="twitter:title" content={title} />
   {/if}
 
   {#if description}
     <meta name="description" content={description} />
     <meta property="og:description" content={description} />
-    <meta name="twitter:description" content={description} />
   {/if}
 
   {#if image}
     <meta property="og:image" content={image} />
-    <meta name="twitter:image" content={image} />
   {/if}
 </svelte:head>
